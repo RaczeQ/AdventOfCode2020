2e7a8dacc1404c552781fb56a2803665fbc97843
RaczeQ <raczyckikamil@gmail.com>
2020-12-12 11:56:27 +0100

    Added Day 12 solvers

---
+++ Day12/FirstSolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ElvenTools;
+
+namespace Day12
+{
+    // What is the Manhattan distance between that location and the ship's starting position?
+    public class FirstSolver : ISolver
+    {
+        public long Calculate(List<string> input)
+        {
+            var directions = new[] {'N', 'E', 'S', 'W'};
+            var distances = new[] {0, 0, 0, 0};
+            var currentDirIdx = 1;
+
+            input.Select(l => (l[0], int.Parse(l[1..]))).ToList().ForEach(t =>
+            {
+                switch (t.Item1)
+                {
+                    case 'L':
+                        currentDirIdx = ((4 - (t.Item2 / 90)) + currentDirIdx) % 4;
+                        break;
+                    case 'R':
+                        currentDirIdx = (currentDirIdx + (t.Item2 / 90)) % 4;
+                        break;
+                    case 'F':
+                        distances[currentDirIdx] += t.Item2;
+                        break;
+                    default:
+                        distances[Array.FindIndex(directions, c => c.Equals(t.Item1))] += t.Item2;
+                        break;
+                }
+            });
+
+            return Math.Abs(distances[0] - distances[2]) + Math.Abs(distances[1] - distances[3]);
+        }
+    }
+}
---
+++ Day12/SecondSolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ElvenTools;
+
+namespace Day12
+{
+    // What is the Manhattan distance between that location and the ship's starting position?
+    public class SecondSolver : ISolver
+    {
+        public long Calculate(List<string> input)
+        {
+            var directions = new[] { 'N', 'E', 'S', 'W' };
+            var shipDistances = new[] { 0, 0, 0, 0 };
+            var waypointDistances = new[] { 1, 10, 0, 0 };
+
+            input.Select(l => (l[0], int.Parse(l[1..]))).ToList().ForEach(t =>
+            {
+                var sliceIdx = t.Item2 / 90;
+                switch (t.Item1)
+                {
+                    case 'L':
+                        waypointDistances = waypointDistances[sliceIdx..].Concat(waypointDistances[0..sliceIdx]).ToArray();
+                        break;
+                    case 'R':
+                        waypointDistances = waypointDistances[^sliceIdx..^0].Concat(waypointDistances[..^sliceIdx]).ToArray();
+                        break;
+                    case 'F':
+                        shipDistances = shipDistances.Zip(waypointDistances, (x, y) => x + y * t.Item2).ToArray();
+                        break;
+                    default:
+                        waypointDistances[Array.FindIndex(directions, c => c.Equals(t.Item1))] += t.Item2;
+                        break;
+                }
+            });
+            return Math.Abs(shipDistances[0] - shipDistances[2]) + Math.Abs(shipDistances[1] - shipDistances[3]);
+        }
+    }
+}
